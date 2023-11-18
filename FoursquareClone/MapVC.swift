1e4fc779a2c37a343ece1787de92f36201e1db05
Александр Федоткин <fedotkin.00@inbox.ru>
2023-11-18 23:44:56 +0700

    Added the function of setting pins and annotations

--- FoursquareClone/MapVC.swift
+++ FoursquareClone/MapVC.swift
@@ -11,13 +11,16 @@ import MapKit
 class MapVC: UIViewController {
     @IBOutlet private weak var mapView: MKMapView!
     private var locationManager = CLLocationManager()
+    private var chosenLatitude = ""
+    private var chosenLongitude = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initialiseNavigationButton()
         setup()
+        addRecognizer()
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         locationManager.stopUpdatingLocation()
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
@@ -38,6 +41,32 @@ extension MapVC : MKMapViewDelegate, CLLocationManagerDelegate {
     }
 }
 
+//MARK: - add recognizer + choseLocation
+private extension MapVC {
+
+    func addRecognizer(){
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(choseLocation(gestureRecognizer: )))
+        recognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(recognizer)
+    }
+
+    @objc func choseLocation(gestureRecognizer: UIGestureRecognizer){
+        if gestureRecognizer.state == UIGestureRecognizer.State.began{
+            
+            let touches = gestureRecognizer.location(in: self.mapView)
+            let coordinates = self.mapView.convert(touches, toCoordinateFrom: self.mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates
+            annotation.title = PlaceModel.sharedInstance.placeName
+            annotation.subtitle = PlaceModel.sharedInstance.placeType
+            self.mapView.addAnnotation(annotation)
+            
+            self.chosenLatitude = String(coordinates.latitude)
+            self.chosenLongitude = String(coordinates.longitude)
+        }
+    }
+}
+
 //MARK: - style for navigationController
 private extension MapVC{
 
@@ -45,11 +74,11 @@ private extension MapVC{
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(title: "Save", style: UIBarButtonItem.Style.plain, target: self, action: #selector(saveButtonClicked))
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "< Back", style: UIBarButtonItem.Style.plain, target: self, action: #selector(backButtonClicked))
     }
-    
+
     @objc func saveButtonClicked(){
         
     }
-    
+
     @objc func backButtonClicked(){
         self.dismiss(animated: true)
     }
