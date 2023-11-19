50c822e2a079b4ffca926e7e4cfac3363abfc232
Александр Федоткин <fedotkin.00@inbox.ru>
2023-11-19 20:45:48 +0700

    Add pin button and open system maps

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import Parse
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController{
     @IBOutlet private weak var detailsImageView: UIImageView!
     @IBOutlet private weak var placeNameLabel: UILabel!
     @IBOutlet private weak var placeTypeLabel: UILabel!
@@ -23,6 +23,7 @@ class DetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getDataFromParse()
+        setup()
     }
     
 }
@@ -42,6 +43,8 @@ private extension DetailsVC {
             } else {
                 if objects != nil {
                     for object in objects! {
+                        //OBJECTS
+                        
                         if let placeName = object["name"] as? String {
                             self.placeNameLabel.text = placeName
                         }
@@ -66,6 +69,76 @@ private extension DetailsVC {
                                 }
                             }
                         }
+                        
+                        // MAPS
+                        self.settingMap()
+                    }
+                }
+            }
+        }
+    }
+}
+
+//MARK: - setup mapDelegate
+extension DetailsVC : MKMapViewDelegate {
+    func setup(){
+        detailsMapView.delegate = self
+    }
+}
+
+//MARK: - Maps zoom and annotation
+private extension DetailsVC{
+
+    func settingMap(){
+        let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.detailsMapView.setRegion(region, animated: true)
+        addAnotation(location: location)
+    }
+    
+    func addAnotation(location: CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = self.placeNameLabel.text
+        annotation.subtitle = self.placeTypeLabel.text
+        self.detailsMapView.addAnnotation(annotation)
+    }
+}
+
+//MARK: - Add pin button and open maps
+extension DetailsVC {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            let button = UIButton(type: .detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.chosenLongitude != 0.0 && self.chosenLatitude != 0.0 {
+            let requestLocation = CLLocation(latitude: chosenLatitude, longitude: chosenLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { placemarks, error in
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        let mkPlaceMark = MKPlacemark(placemark: placemark[0])
+                        let mapItem = MKMapItem(placemark: mkPlaceMark)
+                        mapItem.name = self.placeNameLabel.text
+                        
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+                        mapItem.openInMaps(launchOptions: launchOptions)
                     }
                 }
             }
